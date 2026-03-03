8862b53e86d2d1d61fe43b271d5a6d74681adf1e
Fco Gabriel <fco.gabrielsilva2408@gmail.com>
2026-03-03 12:11:49 -0300

    fix: add timestamps to TransactionCategory

--- src/infra/database/typeorm/dt-money/entities/TransactionCategory.ts
+++ src/infra/database/typeorm/dt-money/entities/TransactionCategory.ts
@@ -1,10 +1,13 @@
 import {
   Column,
+  CreateDateColumn,
+  DeleteDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from "typeorm";
 import { Transaction } from "./Transaction";
 import { User } from "./User";
@@ -23,6 +26,27 @@ export class TransactionCategory {
   // nullable: true porque as categorias padrão (do seeder) não tem dono
   @Column({ name: "user_id", type: "int", nullable: true })
   userId?: number;
+
+  @CreateDateColumn({
+    name: "created_at",
+    type: "datetime",
+    nullable: false, // Pode usar default: "CURRENT_TIMESTAMP" dependendo da sua config
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: "updated_at",
+    type: "datetime",
+    nullable: false,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: "deleted_at",
+    type: "datetime",
+    nullable: true,
+  })
+  deletedAt?: Date | null;
 
   @ManyToOne(() => User)
   @JoinColumn({ name: "user_id", referencedColumnName: "id" })
